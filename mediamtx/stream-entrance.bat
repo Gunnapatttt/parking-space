2856db3f7fe6d4dc0c159c3979b37a266dd1e2f5
gunnapatttt <tptcnclog@gmail.com>
2025-12-21 23:44:23 +0700

    Remove MediaMTX components - using direct RTSP links
    - Remove entire mediamtx/ directory (not needed for direct RTSP)
    - Remove start-streaming.bat (MediaMTX related)
    - Update .gitignore to remove MediaMTX references
    - Simplified to React app + HLS server only

--- mediamtx/stream-entrance.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Streaming video to MediaMTX entrance stream (rtsp://localhost:8554/entrance)
-echo Press Ctrl+C to stop
-echo.
-
-ffmpeg -re -stream_loop -1 -i "../video/FAKE Taylormade P790 from TEMU _ INCREDIBLE RESULTS.mp4" -c:v libx264 -preset fast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 128k -ac 2 -ar 44100 -f rtsp rtsp://localhost:8554/entrance
-
-pause
